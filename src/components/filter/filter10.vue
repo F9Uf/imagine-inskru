c612eec94f814b0028ef2bcc3c7f9bd5ff42d02d
Nopanun Laochunhanun <nopanun@thaenalpha.com>
2019-07-28 01:21:38 +0700

    components/filter/filter10: update

--- src/components/filter/filter10.vue
+++ src/components/filter/filter10.vue
@@ -1,78 +1,38 @@
 <template>
   <div class="filter10">
-    <h3>10.  นักเรียนที่มีความต้องการพิเศษ</h3>
-    <div class="form-check">
+    <h3>10. นักเรียนที่มีความต้องการพิเศษ</h3>
+    <div class="form-check" v-for="(item, index) in list" :key="index">
       <input
         class="form-check-input"
-        type="radio"
-        name="extraRequireRadios"
-        id="extraRequireRadios1"
-        value="ok"
-        v-model="radioBox"
+        type="checkbox"
+        :value="item.label"
+        :id="'defaultCheck'+index"
       />
-      <label class="form-check-label" for="extraRequireRadios1">ปกติ</label>
-    </div>
-    <div class="form-check">
-      <input
-        class="form-check-input"
-        type="radio"
-        name="extraRequireRadios"
-        id="extraRequireRadios2"
-        value="risk"
-        v-model="radioBox"
-      />
-      <label class="form-check-label" for="extraRequireRadios2">เสี่ยง</label>
-    </div>
-    <div class="form-check">
-      <input
-        class="form-check-input"
-        type="radio"
-        name="extraRequireRadios"
-        id="extraRequireRadios3"
-        value="problem"
-        v-model="radioBox"
-      />
-      <label class="form-check-label" for="extraRequireRadios3">มีปัญหา</label>
-    </div>
-    <div v-if="radioBox === 'risk'">
-      <div class="form-check" v-for="(item, index) in risk" :key="index">
-        <input class="form-check-input" type="checkbox" :value="item.label" :id="'defaultCheck'+index">
-        <label class="form-check-label" :for="'defaultCheck'+index">
-          {{item.label}}
-        </label>
-      </div>
-    </div>
-    <div v-if="radioBox === 'problem'">
-      <div class="form-check" v-for="(item, index) in problem" :key="index">
-        <input class="form-check-input" type="checkbox" :value="item.label" :id="'defaultCheck'+index">
-        <label class="form-check-label" :for="'defaultCheck1'+index">
-          {{item.label}}
-        </label>
-      </div>
+      <label class="form-check-label" :for="'defaultCheck1'+index">{{item.label}}</label>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            list: [
-              {label:'มีความบกพร่องทางการเห็น'},
-              {label:'มีความบกพร่องทางการได้ยิน'},
-              {label:'มีความบกพร่องทางสติปัญญา'},
-              {label:'มีความบกพร่องทางร่างกายและสุขภาพ'},
-              {label:'มีความบกพร่องทางการเรียนรู้'},
-              {label:'มีความบกพร่องทางพฤติกรรมหรืออารมณ์'},
-              {label:'มีความบกพร่องทางการพูดและภาษา'},
-              {label:'ออทิสติก'},
-              {label:'มีสมาธิสั้น'},
-              {label:'พิการซ้ำซ้อน (มีความบกพร่องตั้งแต่ 2 ประเภทขึ้นไป)'},
-              {label:'อื่นๆ'}
-            ]
-        }
-    }
-}
+  data() {
+    return {
+      list: [
+        { label: "มีความบกพร่องทางการเห็น" },
+        { label: "มีความบกพร่องทางการได้ยิน" },
+        { label: "มีความบกพร่องทางสติปัญญา" },
+        { label: "มีความบกพร่องทางร่างกายและสุขภาพ" },
+        { label: "มีความบกพร่องทางการเรียนรู้" },
+        { label: "มีความบกพร่องทางพฤติกรรมหรืออารมณ์" },
+        { label: "มีความบกพร่องทางการพูดและภาษา" },
+        { label: "ออทิสติก" },
+        { label: "มีสมาธิสั้น" },
+        { label: "พิการซ้ำซ้อน (มีความบกพร่องตั้งแต่ 2 ประเภทขึ้นไป)" },
+        { label: "อื่นๆ" }
+      ]
+    };
+  }
+};
 </script>
 
 <style>
