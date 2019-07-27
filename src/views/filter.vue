b87558c09309047489078a9a5bd27d3bb94fbd91
F9Uf <atoseka@gmail.com>
2019-07-27 23:13:37 +0700

    created filter3

--- src/views/filter.vue
+++ src/views/filter.vue
@@ -2,15 +2,20 @@
     <div class="filter">
         <h1>filter page</h1>
         <filter1></filter1>
+        <filter2></filter2>
+        <filter3></filter3>
     </div>
 </template>
 
 <script>
 import filter1 from '../components/filter/filter1'
+import filter2 from '../components/filter/filter2'
+import filter3 from '../components/filter/filter3'
 
 export default {
     components: {
-        filter1
+        filter1,
+        filter3
     }
 }
 </script>
