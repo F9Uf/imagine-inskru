a984818effa664e055444a0c58e82e7cc196ff8d
neconone <p.chaiolan30@gmail.com>
2019-07-27 22:18:00 +0700

    cd

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,9 @@
 <template>
   <div id="app">
-<<<<<<< HEAD
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/personal">Personal</router-link>
-=======
     <div class="container">
       <div class="row">
         <router-view/>
       </div>
->>>>>>> a1fd90d6267b7e2755b4e6463602202e6809cbb8
     </div>
   </div>
 </template>
