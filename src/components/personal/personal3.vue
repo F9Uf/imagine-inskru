d402a09b1910f294bf9081b03ff61f0a0ad7a423
pongpat11 <pongpatsitiputa@gmail.com>
2019-07-27 23:31:40 +0700

    per34

---
+++ src/components/personal/personal3.vue
@@ -0,0 +1,34 @@
+<template>
+    <form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">personal3</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+  </div>
+  <div class="form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+</template>
+
+<script>
+const firebase = require('../../service/firebaseConfig');
+
+export default {
+  name: 'personal1',
+  mounted() {
+    console.log(firebase);
+    
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/personal/personal4.vue
@@ -0,0 +1,53 @@
+<template>
+  <form>
+    <div class="form-group">
+      <label for="Cd">
+        Congenital disease:
+        <input type="text" class="form-control" id="Cd" />
+      </label>
+    </div>
+    <div class="form-group">
+      <label for="ad">
+        เเพ้ยา:
+        <input type="text" class="form-control" id="ad" />
+      </label>
+    </div>
+    <div class="form-group">
+      <label for="weight">
+        Weight:
+        <input type="text" class="form-control" id="weight" placeholder="Kilograms" />
+      </label>
+    </div>
+    <div class="form-group">
+      <label for="height">
+        Height:
+        <input type="text" class="form-control" id="height" placeholder="Centimetres" />
+      </label>
+    </div>
+    <div class="form-group">
+      <label for="Cd">
+        Congenital disease:
+        <input type="text" class="form-control" id="Cd" />
+      </label>
+    </div>
+    <div class="form-check">
+      <label class="form-check-label">
+        <input type="radio" class="form-check-input" name="optradio" />Option 1
+      </label>
+    </div>
+  </form>
+</template>
+
+<script>
+const firebase = require("../../service/firebaseConfig");
+
+export default {
+  name: "personal1",
+  mounted() {
+    console.log(firebase);
+  }
+};
+</script>
+
+<style>
+</style>
