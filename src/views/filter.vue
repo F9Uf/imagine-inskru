f90db4f9c1b783af463f85c0de7f96d07b5d0bb7
F9Uf <atoseka@gmail.com>
2019-07-27 23:20:39 +0700

    created filter5

--- src/views/filter.vue
+++ src/views/filter.vue
@@ -4,6 +4,7 @@
         <filter1></filter1>
         <filter2></filter2>
         <filter3></filter3>
+        <filter5></filter5>
         <filter7></filter7>
     </div>
 </template>
@@ -12,13 +13,15 @@
 import filter1 from '../components/filter/filter1'
 import filter2 from '../components/filter/filter2'
 import filter3 from '../components/filter/filter3'
+import filter5 from '../components/filter/filter5'
 import filter7 from '../components/filter/filter7'
 
 export default {
     components: {
         filter1,
         filter3,
-        filter7
+        filter5,
+        filter7,
     }
 }
 </script>
