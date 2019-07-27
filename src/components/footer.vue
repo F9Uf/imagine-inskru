f893d54a7632f4ea4a891e19e2bcd5f98d073639
F9Uf <atoseka@gmail.com>
2019-07-28 04:04:27 +0700

    edit footer and clear cache

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -2,7 +2,9 @@
     <nav class="footer">
         <ul class="nav bg-success py-2" >
             <li class="nav-item mx-auto">
-                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true" style="color: white;">Inskru Hackathon | Imagine Team</a>
+                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true" style="color: white;">
+                    Inskru Hackathon | Imagine Team 2019
+                </a>
             </li>
         </ul>
     </nav>
