030ae1544f2edc2c5d67f7a8440f81e3ddaac4cf
neconone <p.chaiolan30@gmail.com>
2019-07-27 22:16:17 +0700

    personal

---
+++ src/views/Personal.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="personal">
+      <personal></personal>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import personal from '@components/personal/personal1.vue'
+
+export default {
+  name: 'personal',
+  components: {
+    personal
+  }
+}
+</script>
+``
