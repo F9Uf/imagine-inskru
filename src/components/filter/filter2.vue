ddeb3bf11565df209523a9c7530bd54b8e99c423
Nopanun Laochunhanun <nopanun@thaenalpha.com>
2019-07-27 22:29:34 +0700

    add filter2 file

---
+++ src/components/filter/filter2.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="filter2">
+    <h3>ด้านการเรียน</h3>
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" value id="defaultCheck1" />
+      <label class="form-check-label" for="defaultCheck1">Default checkbox</label>
+    </div>
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" value id="defaultCheck2" disabled />
+      <label class="form-check-label" for="defaultCheck2">Disabled checkbox</label>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
