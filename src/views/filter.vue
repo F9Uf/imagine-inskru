35033e67f5b4d24642f0c6ecba224999c24b7091
F9Uf <atoseka@gmail.com>
2019-07-27 23:23:36 +0700

    created filter9

--- src/views/filter.vue
+++ src/views/filter.vue
@@ -1,11 +1,12 @@
 <template>
-    <div class="filter">
+    <div class="filter mb-5">
         <h1>filter page</h1>
         <filter1></filter1>
         <filter2></filter2>
         <filter3></filter3>
         <filter5></filter5>
         <filter7></filter7>
+        <filter9></filter9>
     </div>
 </template>
 
@@ -15,6 +16,7 @@ import filter2 from '../components/filter/filter2'
 import filter3 from '../components/filter/filter3'
 import filter5 from '../components/filter/filter5'
 import filter7 from '../components/filter/filter7'
+import filter9 from '../components/filter/filter9'
 
 export default {
     components: {
@@ -22,6 +24,7 @@ export default {
         filter3,
         filter5,
         filter7,
+        filter9
     }
 }
 </script>
