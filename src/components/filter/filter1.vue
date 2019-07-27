09c2c746e00108800863114b6102f8d2004698b1
F9Uf <atoseka@gmail.com>
2019-07-27 22:29:10 +0700

    add filter page

--- src/components/filter/filter1.vue
+++ src/components/filter/filter1.vue
@@ -1,12 +1,44 @@
 <template>
-    <div class="row">
-        <h1>ความสามารถพิเศษ</h1>
+    <div class="filter1">
+        <h3>ความสามารถพิเศษ</h3>
+        <div class="form-check">
+            <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="yes" v-model="radioBox">
+            <label class="form-check-label" for="exampleRadios1">
+            มี
+            </label>
+        </div>
+            <div class="form-check">
+            <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="no" v-model="radioBox">
+            <label class="form-check-label" for="exampleRadios2">
+            ไม่มี
+            </label>
+        </div>
+        <div class="form-contro" v-if="radioBox === 'yes'">
+            <div class="form-group">
+                <input type="text" class="form-control" placeholder="คณิตศาสตร์">
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-
+    data() {
+        return {
+            radioBox: null,
+            form: {
+                math: '',
+                thai: '',
+                eng: '',
+                sci: '',
+                art: '',
+                tech: '',
+                sport: '',
+                social: '',
+                etc: ''
+            }
+        }
+    }
 }
 </script>
 
---
+++ src/views/filter.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="filter">
+        <h1>filter page</h1>
+        <filter1></filter1>
+    </div>
+</template>
+
+<script>
+import filter1 from '../components/filter/filter1'
+
+export default {
+    components: {
+        filter1
+    }
+}
+</script>
+
+<style>
+
+</style>
